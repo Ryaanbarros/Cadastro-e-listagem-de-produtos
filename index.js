4f05aedda21fed98dbc1fef65db774d6896e6e65
Ryan Barros <ryan.santos.barros@hotmail.com>
2024-03-22 23:12:09 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-const produtoForm = document.getElementById('produtoForm');
+const produtoFormu = document.getElementById('produtoFormu');
     const listaProdutos = document.getElementById('listaProdutos');
-    const produtos = []; // Array para armazenar os produtos
+    const produtos = [];
 
-    produtoForm.addEventListener('submit', function (event) {
+    produtoFormu.addEventListener('submit', function (event) {
         event.preventDefault();
 
         const nome = document.getElementById('nome').value;
         const valor = parseFloat(document.getElementById('valor').value).toFixed(2);
 
-        // Adicionar o novo produto ao array
         produtos.push({ nome, valor });
 
-        // Ordenar os produtos por valor (do menor para o maior)
         produtos.sort((a, b) => a.valor - b.valor);
 
-        // Atualizar a lista de produtos
-        listaProdutos.innerHTML = ''; // Limpar a lista
+        listaProdutos.innerHTML = ''; 
         produtos.forEach(produto => {
             const novoProduto = document.createElement('li');
             novoProduto.textContent = `${produto.nome} - R$ ${produto.valor}`;
             listaProdutos.appendChild(novoProduto);
         });
 
-    // Limpar campos do formulário
-    produtoForm.reset();
+    produtoFormu.reset();
 });
